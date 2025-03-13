355be43d00269b1f3a213aa1bf79e43fdfa72a85
moustik <starlinator06@live.fr>
2025-03-13 14:11:45 +0100

    refactor(api): Ajout data buffer Chunk.js

--- packages/api/models/Chunk.js
+++ packages/api/models/Chunk.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
-const pixelSchema = require("./Pixel");
 
 const chunkSchema = new mongoose.Schema({
 	x: { type: Number, required: true },
 	y: { type: Number, required: true },
-	pixels: [[pixelSchema]],
+	// Référence au board parent
+	boardId: { type: mongoose.Schema.Types.ObjectId, ref: 'PixelBoard', required: true },
+	// Array binaires pour stocker les pixels, même méthode que https://redditinc.com/blog/how-we-built-rplace
+	data: { type: Buffer, required: true },
 	lastUpdated: { type: Date, default: Date.now },
 });
 
+// Index unique pour garantir qu'il n'y a pas de doublons
 chunkSchema.index({ boardId: 1, x: 1, y: 1 }, { unique: true });
 
 module.exports = mongoose.model("Chunk", chunkSchema);
