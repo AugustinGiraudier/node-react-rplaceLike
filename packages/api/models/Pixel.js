7f5c35f0ac17bad248723cb1017ec8cc431f1237
moustik <starlinator06@live.fr>
2025-03-13 14:12:07 +0100

    refactor(api): Delete Pixel.js

--- packages/api/models/Pixel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const pixelSchema = new mongoose.Schema({
-	color: { type: String, required: true, default: '#FFFFFF' },
-	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-	timestamp: { type: Date, default: null }
-}, { _id: false }); // Pas besoin d'ID pour chaque pixel individuel
-
-module.exports = pixelSchema;
