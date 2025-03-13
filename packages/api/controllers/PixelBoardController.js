458057d9968894ebfd715c999a9dd43f8a84ac7a
moustik <starlinator06@live.fr>
2025-03-13 14:12:33 +0100

    feat(api): Nouvelles routes + controller PixelBoardController.js board.routes.js

--- packages/api/controllers/PixelBoardController.js
+++ packages/api/controllers/PixelBoardController.js
@@ -18,3 +18,43 @@ exports.createBoard = async (req, res) => {
 		res.status(400).json({ message: 'Error when creating the board : ' + error });
 	}
 };
+
+exports.getRegion = async (req, res) => {
+	try {
+		const { boardId, startX, startY, width, height } = req.params;
+		const region = await BoardService.getRegion(boardId, startX, startY, width, height);
+		res.json(region);
+	} catch (error) {
+		res.status(500).json({ message: 'Server error : ' + error });
+	}
+};
+
+exports.getChunk = async (req, res) => {
+	try {
+		const { boardId, pixelX, pixelY } = req.params;
+		const chunk = await BoardService.getChunk(boardId, pixelX, pixelY);
+		res.json(chunk);
+	} catch (error) {
+		res.status(500).json({ message: 'Server error : ' + error });
+	}
+};
+
+exports.updatePixel = async (req, res) => {
+	try {
+		const { boardId, pixelX, pixelY, color,userId } = req.body;
+		const updated = await BoardService.updatePixel(boardId, pixelX, pixelY, color,userId);
+		res.json(updated);
+	} catch (error) {
+		res.status(500).json({ message: 'Server error : ' + error });
+	}
+};
+
+exports.deleteBoard = async (req, res) => {
+	try {
+		const { boardId } = req.body;
+		const deleted = await BoardService.deleteBoard(boardId);
+		res.json(deleted);
+	} catch (error) {
+		res.status(500).json({ message: 'Server error : ' + error });
+	}
+};
--- packages/api/routes/board.routes.js
+++ packages/api/routes/board.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { getBoards, createBoard } = require('../controllers/PixelBoardController');
+const { getBoards, createBoard,getChunk,getRegion,updatePixel,deleteBoard } = require('../controllers/PixelBoardController');
 
 const router = express.Router();
 
 router.get('/', getBoards);
 router.post('/', createBoard);
-
+router.get('/region/:boardId/:startX/:startY/:width/:height', getRegion);
+router.get('/chunk/:boardId/:pixelX/:pixelY', getChunk);
+router.post('/update', updatePixel);
+router.delete('/:boardId', deleteBoard);
 module.exports = router;
