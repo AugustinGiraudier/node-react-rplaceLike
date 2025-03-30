e441feaebfce69ea49b754686a86850d27ec18bd
qescobar <quentin.escobar@etu.univ-cotedazur.fr>
2025-03-30 10:53:33 +0200

    feat(api): User Stats

--- packages/api/routes/stats.routes.js
+++ packages/api/routes/stats.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {getStats} = require("../controllers/statsController");
+const {getStats,getUserStats} = require("../controllers/statsController");
 
 const router = express.Router();
 
 router.get("/", getStats);
+router.get("/user/:userId", getUserStats);
 
 module.exports = router;
